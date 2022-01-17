bedc1e9741d97de3bd168afe918fb199d2d1705e
Kori Ryter <kryter@gmail.com>
2022-01-17 11:32:20 -0600

    Update based on Barnstorm core

--- cypress/barnstorm/useInstruments.ts
+++ cypress/barnstorm/useInstruments.ts
@@ -1,8 +1,8 @@
-import { InstrumentSet } from "@kryter/barnstorm/lib/InstrumentSet";
+import { useInstrumentSet, InstrumentSet } from "@kryter/barnstorm/lib/InstrumentSet";
 import { useMechanics } from "@kryter/barnstorm-cypress/lib/useMechanics";
 
 const mechanicGroup = useMechanics();
 
 export function useInstruments(): InstrumentSet {
-  return new InstrumentSet(mechanicGroup);
+  return useInstrumentSet(mechanicGroup);
 }
